23fd61d552e3c890b69af544217c3cfa9a8e3b7a
Chelsea G <chelseagieck@Chelseas-MacBook-Pro.local>
2018-03-30 14:56:47 -0400

    better

--- main.js
+++ main.js
@@ -1,53 +1,98 @@
-var topics = ["dog", "cat","dinosaur", "unicorn", "hedgehog", "lion", "tiger", "bear", "monkey", "goat", "turtle", "shark", "dolphin"];
+var topics = ["dog", "cat","dinosaur", "unicorn", "hedgehog", "lion", "tiger", "bear", "monkey", "goat", "turtle", "dolphin"];
 var userInput = "";
+var buttons = "";
+var customInput = "";
+var url = "https://api.giphy.com/v1/gifs/search"
+var search = "";
+var buttonDiv = $('#buttonDiv');
+var imageDiv = $('#imageDiv');
+var buttonCount = 0;
 
-var createButtons = function(){
-    for (i = 0; i < topics.length; i++){
-        $('#buttonDiv').append(
-            "<button class = 'btn btn-dark m-1'>" + topics[i] + "</button>"
-        );
+// var createButton = function(){
+//     for(i = 0; i< topics.length; i++){
+//         $('#buttonDiv').append(
+//             "<button class='btn btn-dark btn-lrg m-1' id='buttonTopic'>" + topics[i] + "</button>"
+//         )
+//         userInput = topics[i];
+//         console.log(userInput)
+//     }
+// }
+
+var createButton = function(){
+    for(i = 0; i< topics.length; i++){
+        $('#buttonDiv').append("<button class='btn btn-dark btn-lrg m-1 buttonTopic' id = "+ buttonCount + ">" + topics[i] + "</button>");
+        buttonCount++;
+        var allTopics = topics[i];
+        console.log(allTopics);
     };
 };
 
 $( document ).ready(function() {
     console.log( "ready!" );
+    createButton();
     
-    createButtons();
+    
+$("#userSearch").click(function(event){
+    event.preventDefault();
+        // Link to what user puts in search box
+        var search = $("#searchTerm").val();
+        console.log(search);
+        topics.push(search);
+        console.log(topics);
+        var userChoice = search;
+        $('#buttonDiv').append("<button class='btn btn-dark btn-lrg m-1 buttonTopic' id = " + buttonCount + ">" + userChoice + "</button>");
+        buttonCount++;
 
-});
- /*
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        userInput + "&api_key=wlD16JyZKmFHTkFbwCyx2SRxbMKnEXNJ";
+        $('.buttonTopic').on("click", this, function(event){
+            console.log("inside")
+            var userChoice = event.target.id;
+            console.log(userChoice);
+            var userInput = topics[userChoice];
+            console.log(userInput);
+        });
+
+    
+}); // end of search
+
+
+$('.buttonTopic').on("click", this, function(event){
+    console.log("inside")
+    var userChoice = event.target.id;
+    console.log(userChoice);
+    var userInput = topics[userChoice];
+    console.log(userInput);
+    
+    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userInput +"&limit=10" + "&api_key=wlD16JyZKmFHTkFbwCyx2SRxbMKnEXNJ";
     // Performing our AJAX GET request
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-        // After the data comes back from the API
-        .then(function(response) {
-          // Storing an array of results in the results variable
-          var results = response.data;
-          // Looping over every result item
-          for (var i = 0; i < results.length; i++) {
-            // Only taking action if the photo has an appropriate rating
-            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-              // Creating a div with the class "item"
-              var gifDiv = $("<div class='item'>");
-              // Storing the result item's rating
-              var rating = results[i].rating;
-              // Creating a paragraph tag with the result item's rating
-              var p = $("<p>").text("Rating: " + rating);
-              // Creating an image tag
-              var personImage = $("<img>");
-              // Giving the image tag an src attribute of a proprty pulled off the
-              // result item
-              personImage.attr("src", results[i].images.fixed_height.url);
-              // Appending the paragraph and personImage we created to the "gifDiv" div we created
-              gifDiv.append(p);
-              gifDiv.append(personImage);
-              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-              $("#gifs-appear-here").prepend(gifDiv);
-            }
-          }
-        });
-*/
+    // After the data comes back from the API
+    .then(function(response) {
+    // Storing an array of results in the results variable
+    var results = response.data;
+    console.log(results);
+
+    for (var i = 0; i < results.length; i++) {
+        // Only taking action if the photo has an appropriate rating
+        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+            console.log(userInput);
+            var rating = results[i].rating;
+        //  console.log(rating);
+            var imageChoice = results[i].images.fixed_height.url;
+            var imagePut = $("<h2> Rating : " + rating + "</h2>" + "<img src = " + imageChoice + ">");
+
+            var imageHolder = $("<div class = " + " ' " + userInput + " ' "+  ">" + "</div>");
+        //  var imageHeader = $("<h1>" + userInput + "</h1>")
+        //  $(imageHolder).append(imageHeader);
+            $(imageHolder).append(imagePut);
+            $('#imageDiv').prepend(imageHolder);
+        }// this is technically end of rating
+    } // end of for statement with results & rating inside
+}) // end of .then
+}); // end button click
+
+
+}); // end doc ready
