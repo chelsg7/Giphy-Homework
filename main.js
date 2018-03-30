f4c10b6e8bf703cfc5348ef6b1a724313463db1e
Chelsea G <chelseagieck@Chelseas-MacBook-Pro.local>
2018-03-30 15:05:50 -0400

    working

--- main.js
+++ main.js
@@ -30,29 +30,7 @@ var createButton = function(){
 $( document ).ready(function() {
     console.log( "ready!" );
     createButton();
-    
-    
-$("#userSearch").click(function(event){
-    event.preventDefault();
-        // Link to what user puts in search box
-        var search = $("#searchTerm").val();
-        console.log(search);
-        topics.push(search);
-        console.log(topics);
-        var userChoice = search;
-        $('#buttonDiv').append("<button class='btn btn-dark btn-lrg m-1 buttonTopic' id = " + buttonCount + ">" + userChoice + "</button>");
-        buttonCount++;
 
-        $('.buttonTopic').on("click", this, function(event){
-            console.log("inside")
-            var userChoice = event.target.id;
-            console.log(userChoice);
-            var userInput = topics[userChoice];
-            console.log(userInput);
-        });
-
-    
-}); // end of search
 
 
 $('.buttonTopic').on("click", this, function(event){
@@ -93,6 +71,57 @@ $('.buttonTopic').on("click", this, function(event){
     } // end of for statement with results & rating inside
 }) // end of .then
 }); // end button click
+
+    
+    
+$("#userSearch").click(function(event){
+    event.preventDefault();
+        // Link to what user puts in search box
+        var search = $("#searchTerm").val();
+        console.log(search);
+        topics.push(search);
+        console.log(topics);
+        var userChoice = search;
+        $('#buttonDiv').append("<button class='btn btn-dark btn-lrg m-1 buttonTopic' id = " + buttonCount + ">" + userChoice + "</button>");
+        buttonCount++;
+
+        $('.buttonTopic').on("click", this, function(event){
+            console.log("inside")
+            var userInput = event.target.id;
+            console.log(userInput);
+            console.log(userChoice);
+        });
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userChoice +"&limit=10" + "&api_key=wlD16JyZKmFHTkFbwCyx2SRxbMKnEXNJ";
+        // Performing our AJAX GET request
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // After the data comes back from the API
+        .then(function(response) {
+            // Storing an array of results in the results variable
+            var results = response.data;
+            console.log(results);
+            for (var i = 0; i < results.length; i++) {
+                // Only taking action if the photo has an appropriate rating
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                    console.log(userInput);
+                    var rating = results[i].rating;
+                    //  console.log(rating);
+                    var imageChoice = results[i].images.fixed_height.url;
+                    var imagePut = $("<h2> Rating : " + rating + "</h2>" + "<img src = " + imageChoice + ">");
+                    var imageHolder = $("<div class = " + " ' " + userInput + " ' "+  ">" + "</div>");
+                    //  var imageHeader = $("<h1>" + userInput + "</h1>")
+                    //  $(imageHolder).append(imageHeader);
+                    $(imageHolder).append(imagePut);
+                    $('#imageDiv').prepend(imageHolder);
+                }// this is technically end of rating
+            } // end of for statement with results & rating inside
+        }) // end of .then
+    }); // end button click
+
+
 
 
 }); // end doc ready
