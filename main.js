151f6c2f5dd5bd6956c47f0afb008b3f05bb037d
Chelsea G <chelseagieck@Chelseas-MacBook-Pro.local>
2018-03-30 15:18:49 -0400

    working

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ $('.buttonTopic').on("click", this, function(event){
     console.log(userInput);
     
     
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userInput +"&limit=10" + "&api_key=wlD16JyZKmFHTkFbwCyx2SRxbMKnEXNJ";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userInput +"&limit=15" + "&api_key=wlD16JyZKmFHTkFbwCyx2SRxbMKnEXNJ";
     // Performing our AJAX GET request
     $.ajax({
         url: queryURL,
@@ -62,7 +62,7 @@ $('.buttonTopic').on("click", this, function(event){
             var imageChoice = results[i].images.fixed_height.url;
             var imagePut = $("<h2> Rating : " + rating + "</h2>" + "<img src = " + imageChoice + ">");
 
-            var imageHolder = $("<div class = " + " ' " + userInput + " ' "+  ">" + "</div>");
+            var imageHolder = $("<div class = " + " 'text-center  " + userInput + " ' "+  ">" + "</div>");
         //  var imageHeader = $("<h1>" + userInput + "</h1>")
         //  $(imageHolder).append(imageHeader);
             $(imageHolder).append(imagePut);
@@ -90,38 +90,37 @@ $("#userSearch").click(function(event){
             var userInput = event.target.id;
             console.log(userInput);
             console.log(userChoice);
-        });
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userChoice +"&limit=10" + "&api_key=wlD16JyZKmFHTkFbwCyx2SRxbMKnEXNJ";
-        // Performing our AJAX GET request
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        // After the data comes back from the API
-        .then(function(response) {
-            // Storing an array of results in the results variable
-            var results = response.data;
-            console.log(results);
-            for (var i = 0; i < results.length; i++) {
-                // Only taking action if the photo has an appropriate rating
-                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-                    console.log(userInput);
-                    var rating = results[i].rating;
-                    //  console.log(rating);
-                    var imageChoice = results[i].images.fixed_height.url;
-                    var imagePut = $("<h2> Rating : " + rating + "</h2>" + "<img src = " + imageChoice + ">");
-                    var imageHolder = $("<div class = " + " ' " + userInput + " ' "+  ">" + "</div>");
-                    //  var imageHeader = $("<h1>" + userInput + "</h1>")
-                    //  $(imageHolder).append(imageHeader);
-                    $(imageHolder).append(imagePut);
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userChoice +"&limit=15" + "&api_key=wlD16JyZKmFHTkFbwCyx2SRxbMKnEXNJ";
+            // Performing our AJAX GET request
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            // After the data comes back from the API
+            .then(function(response) {
+                // Storing an array of results in the results variable
+                var results = response.data;
+                console.log(results);
+                for (var i = 0; i < results.length; i++) {
+                    // Only taking action if the photo has an appropriate rating
+                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                        console.log(userChoice);
+                        var rating = results[i].rating;
+                        //  console.log(rating);
+                        var stillImageChoice = results[i].images.fixed_height_still.url
+                        var imageChoice = results[i].images.fixed_height.url;
+                        var imagePut = $("<img src = " + imageChoice + ">" + "<h2> Rating : " + rating + "</h2>");
+                        var imageHolder = $("<div class = " + " 'text-center  " + userInput + " ' "+  ">" + "</div>");
+                        $(imageHolder).append(imagePut);
+                    // var imageHeader = $("<div class = 'mx-auto'> <h1 class = ' text-center'" +  ">" + userChoice + "</h1> </div>")
+                    //  $(imageHeader).append(imageHolder);
                     $('#imageDiv').prepend(imageHolder);
-                }// this is technically end of rating
-            } // end of for statement with results & rating inside
-        }) // end of .then
-    }); // end button click
+                    }// this is technically end of rating
+                } // end of for statement with results & rating inside
+            }) // end of .then
 
-
-
+        });
+    });
 
 }); // end doc ready
